076f3dd8ee5c11294605bcc7eb431599b09616b7
DanielCabral <daniel-cabral14@hotmail.com>
2020-11-30 01:32:34 -0300

    Less changes

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -1,15 +1,15 @@
 define({
   "name": "LearnEnglish",
   "version": "1.0.0",
-  "description": "LearnEnglish",
+  "description": "Api de acesso as rotas do sistema LearnEnglish",
   "title": "LearnEnglish API",
-  "url": "http://v1",
+  "url": "https://api-learnenglish.herokuapp.com",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-11-29T06:24:52.746Z",
+    "time": "2020-11-30T04:31:44.741Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
