c237d1fde2535a99d1da0781471647760a6f29b1
DanielCabral <daniel-cabral14@hotmail.com>
2020-12-03 12:34:12 -0300

    Cleardb configured

--- knexfile.js
+++ knexfile.js
@@ -3,11 +3,12 @@ module.exports = {
   
     development: {      
       client: 'mysql',
+      //mysql://bb3f30639486b2:862c56b8@us-cdbr-east-02.cleardb.com/heroku_d3a9b580de36c15?reconnect=true
       connection: {
         host: 'us-cdbr-east-02.cleardb.com',
-        database: 'heroku_2b58cdacaab391e',
-        user:     'b0c38f9eb9cb49',
-        password: '9932d5d2'
+        database: 'heroku_d3a9b580de36c15',
+        user:     'bb3f30639486b2',
+        password: '862c56b8'
       },
       pool: {
         min: 2,
@@ -21,10 +22,10 @@ module.exports = {
     staging: {
       client: 'mysql',
       connection: {
-        host: 'db4free.net',
-        database: 'makerblocks',
-        user:     'makerblocks',
-        password: 'abc.1234'
+      host: 'us-cdbr-east-02.cleardb.com',
+      database: 'heroku_d3a9b580de36c15',
+      user:     'bb3f30639486b2',
+      password: '862c56b8'
       },
       pool: {
         min: 2,
@@ -39,10 +40,10 @@ module.exports = {
     production: {
       client: 'mysql',
       connection: {
-        host: 'db4free.net',
-        database: 'makerblocks',
-        user:     'makerblocks',
-        password: 'abc.1234'
+        host: 'us-cdbr-east-02.cleardb.com',
+        database: 'heroku_d3a9b580de36c15',
+        user:     'bb3f30639486b2',
+        password: '862c56b8'
       },
       pool: {
         min: 2,
